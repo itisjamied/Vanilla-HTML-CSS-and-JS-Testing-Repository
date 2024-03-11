2aa3ea8b3f0ec6dc959a02f8f56007783b93b8b1
Jamie De Los Santos <itisjaamied@gmail.com>
2024-03-11 17:27:07 -0400

    updated parcel

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./index.html", // This points to your HTML file at the project root
     "./src/**/*.{html,js}" // This includes all HTML and JS files within the src directory
   ],
-  plugins: [],
+  theme: {
+    extend: {
+      height: {
+        '60screen': '60vh',
+      },
+      colors: {
+        'pross-gray': '#BEBEBE', 
+      },
+      backgroundColor: {
+        'pross-black': '#020202', 
+      },
+    },
+  },
 }
 
