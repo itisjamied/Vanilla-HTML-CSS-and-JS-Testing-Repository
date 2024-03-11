4f8eed6a9f508aadf3189b3139e6d99d92ae3975
Jamie De Los Santos <itisjaamied@gmail.com>
2024-03-11 15:53:57 -0400

    ui test page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      height: {
+        '60screen': '60vh',
+      },
+      colors: {
+        'pross-gray': '#BEBEBE', 
+      },
       backgroundColor: {
         'pross-black': '#020202', 
       },
     },
   },
-  plugins: [],
 }
 
