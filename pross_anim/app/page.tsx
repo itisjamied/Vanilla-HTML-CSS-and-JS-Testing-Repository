0cba56a9d1d849107688cb63cfa421722d2f512b
HandyDorceus <77027304+itisjamied@users.noreply.github.com>
2024-03-20 11:06:20 -0400

    setup

---
+++ pross_anim/app/page.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center text-white p-10 m-4 rounded-lg">
+      <div className="w-1/2 text-center">
+      <header className="mb-4 text-center">
+        <h1>Lorem Ipsum Dolor Sit Amet</h1>
+        <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </header>
+
+      <main className="flex flex-col items-center justify-center mb-4">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </main>
+
+      <footer className="text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </footer>
+      </div>
+    </div>
+  );
+}
---
+++ pross_anim/pages/about.tsx
@@ -0,0 +1,21 @@
+
+
+export default function About() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-center text-white p-10 m-4 rounded-lg">
+        <header className="mb-4 text-center">
+          <h1>Lorem Ipsum Dolor Sit Amet</h1>
+          <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </header>
+  
+        <main className="flex flex-col items-center justify-center mb-4">
+          <p>ABOUT ABOUT ABOUT ABOUT</p>
+        </main>
+  
+        <footer className="text-center">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </footer>
+      </div>
+    );
+  }
+  
